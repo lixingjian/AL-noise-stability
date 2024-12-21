83608cddb6bebdebcc80f80b49cfd0e43ef264bc
lixingjian <1762778193@qq.com>
2024-12-21 17:10:39 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #datasets are located in the directory ./benchmark, e.g. ./benchmark/MNIST
 
 #mnist
-cp config_mnist.py config.py; python -u main.py -d mnist -m NoiseStability -k 30
+cp configs/config_mnist.py config.py; python -u main.py -d mnist -m NoiseStability -k 30
 
 #cifar10
-cp config_cifar10.py config.py; python -u main.py -d cifar10 -m NoiseStability -k 30
+cp configs/config_cifar10.py config.py; python -u main.py -d cifar10 -m NoiseStability -k 30
